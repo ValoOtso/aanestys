0379fe9f626a9b9ee3334b111cdeadec31829a69
Valo Riutta <valo.riutta@gmail.com>
2025-01-28 12:34:27 +0200

    muutoksia

--- script.js
+++ script.js
@@ -1,46 +1,59 @@
 let kayttajanimi
 let salasana
-let valid = true
-const kayttajanimet = []
-const salasanat = []
+let valid
+const kayttajat = []
+let kayttaja = {}
+
+localStorage.setItem('users', JSON.stringify(kayttajat))
+
+let valo = 'valo'
+let pass = '1234'
+
+let esimerkki = {username: valo, password: pass}
+kayttajat.push(esimerkki)
 
 
 function kirjaudu(){
     kayttajanimi = document.getElementById('username2').value
     salasana = document.getElementById('password2').value
-    if(kayttajanimet.indexOf(kayttajanimi) == -1){
-        document.getElementById('login').addEventListener('submit', function(event){
-            event.preventDefault()
-        })
-    }else if(kayttajanimet.indexOf(kayttajanimi) != salasanat.indexOf(salasana)){
-        document.getElementById('login').addEventListener('submit', function(event){
-            event.preventDefault()
-        })
-    }else{
-        //kirjautuminen onnistuu ja siitä tulee viesti sekä kirjautumisikkuna katoaa
-        document.getElementById('kirjautumisilmoitus').innerHTML = 'Kirjautuminen onnistui!'
-        setTimeout(poistaIlmoitus, 10000)
-    }
 
 }
 
 function tallennaTiedot(){
+    valid = true
     kayttajanimi = document.getElementById('username1').value
     salasana = document.getElementById('password1').value
-    if(kayttajanimet.indexOf(kayttajanimi) != -1 || kayttajanimi.length == 0){
-        document.getElementById('signup').addEventListener('submit', function(event){
-            event.preventDefault()
-        })
-        document.getElementById('error-signup').innerHTML = 'Käyttäjänimi on jo olemassa!'
-    }else{
-        kayttajanimet.push(kayttajanimi)
-        salasanat.push(salasana)
-        document.getElementById('kirjautumisilmoitus').innerHTML = 'Rekisteröityminen onnistui!'
-        setTimeout(poistaIlmoitus, 10000)
+    console.log(kayttajanimi)
+    console.log(salasana)
+    kayttaja = {username: kayttajanimi, password: salasana, admin: document.getElementById('admin1').checked}
+    console.log(kayttaja)
+    if(salasana.length === 0 || kayttajanimi.length === 0){
+        valid = false
+        alert('kirjoita salasana')
+        return valid
     }
-    
+    for (let i = 0; i < kayttajat.length; i++){
+        if (kayttajanimi === kayttajat[i]['username']){
+            alert('käyttäjänimi varattu')
+            valid = false
+        }
+        if(i+1 === kayttajat.length){
+            console.log(kayttaja)
+            kayttajat.push(kayttaja)
+            valid = true
+            return valid
+        }
+    }
+    return valid
 }
 
 function poistaIlmoitus(){
     document.getElementById('kirjautumisilmoitus').style.display = 'none'
 }
+
+localStorage.setItem('users', JSON.stringify(kayttajat))
+
+console.log({...localStorage})
+console.log(kayttajat.length)
+
+console.log(kayttajat[0]['username'])
