d399771cbcd95c300018ddb04b98b3d0995e165c
Valo Riutta <valo.riutta@gmail.com>
2025-02-06 11:25:50 +0200

    kayttajat

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 let kayttajanimi
 let salasana
 let valid
-const kayttajat = []
 let kayttaja = {}
 
-localStorage.setItem('users', JSON.stringify(kayttajat))
-
-let valo = 'valo'
-let pass = '1234'
-
-let esimerkki = {username: valo, password: pass}
-kayttajat.push(esimerkki)
 
 
 function kirjaudu(){
@@ -19,42 +11,38 @@ function kirjaudu(){
 
 }
 
+
+
 function tallennaTiedot(){
-    valid = true
+    //if (localStorage.getItem('users') === null){
+        //let kayttajat = []
+    //}else{let kayttajat = localStorage.getItem('users')}
+    const kayttajat = localStorage.getItem('users') || []
     kayttajanimi = document.getElementById('username1').value
     salasana = document.getElementById('password1').value
-    console.log(kayttajanimi)
-    console.log(salasana)
     kayttaja = {'username': kayttajanimi, 'password': salasana, 'admin': document.getElementById('admin1').checked}
-    console.log(kayttaja)
     if(salasana.length === 0 || kayttajanimi.length === 0){
         valid = false
         alert('kirjoita salasana')
-        return valid
+        
     }
-    for (let i = 0; i < kayttajat.length; i++){
-        if (kayttajanimi === kayttajat[i]['username']){
-            alert('käyttäjänimi varattu')
-            valid = false
+    if(kayttajat.length > 0){
+        for (let i = 0; i < kayttajat.length; i++){
+            if (kayttajanimi === kayttajat[i]['username']){
+                alert('käyttäjänimi varattu')
+                break
+            }
+            if(i+1 === kayttajat.length){
+                kayttajat.push(kayttaja)
+                localStorage.setItem('users', JSON.stringify(kayttajat))
+            }
         }
-        if(i+1 === kayttajat.length){
-            console.log(kayttaja)
-            kayttajat.push(kayttaja)
-            localStorage.setItem('users', JSON.stringify(kayttajat))
-            valid = true
-            return valid
-        }
+    }else{
+        kayttajat.push(kayttaja)
+                localStorage.setItem('users', JSON.stringify(kayttajat))
     }
-    return valid
 }
 
 function poistaIlmoitus(){
     document.getElementById('kirjautumisilmoitus').style.display = 'none'
 }
-
-localStorage.setItem('users', JSON.stringify(kayttajat))
-
-console.log({...localStorage})
-console.log(kayttajat.length)
-
-console.log(kayttajat[0]['username'])
